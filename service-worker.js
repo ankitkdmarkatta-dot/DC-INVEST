69b680439da669702af6a818189c854dead01091
ankitkdmarkatta-dot <ankitkdmarkatta@gmail.com>
2026-03-20 11:09:19 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,18 @@
 const CACHE_NAME = 'dc-invest-v1';
+const assets = [
+  '/',
+  '/home.html',
+  '/login.html',
+  '/logo.png',
+  // Yahan apni baki CSS/JS files ke naam bhi add karein
+];
 
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installing...');
-});
-
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker: Activated');
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(assets);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
